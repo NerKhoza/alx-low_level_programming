6aa65dd54b87b73a1da0a1cb9e61e3e58bc915fe
NerKhoza <Khoza.ner.142@gmail.com>
2023-09-08 17:43:08 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char al;
+
+	for (al = 'z' ; al <= 'a' ; al--)
+		putchar(al);
+	putchar('\n');
+	return (0);
+}
