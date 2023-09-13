f479de10302a78e86005b051646d620a4e896581
NerKhoza <Khoza.ner.142@gmail.com>
2023-09-13 05:26:39 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet in lower case 10X
+ *
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char x;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (x = 'a' ; x <= 'z' ; x++)
+			_putchar(x);
+		_putchar('\n');
+	}
+}
