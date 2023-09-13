436d70d5642f4196bbb3cc3d856783ad08a068cb
NerKhoza <Khoza.ner.142@gmail.com>
2023-09-13 12:51:54 -0700

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - function that takes the absolute value of a number
+ * @x: function parameter
+ * Return: -a or a
+ */
+
+int _abs(int x)
+{
+	if (x < 0)
+		return (-x);
+	else
+		return (x);
+}
