e10d741dd8ce388d441d93d879f7d4e5f02446bf
NerKhoza <Khoza.ner.142@gmail.com>
2023-10-01 13:25:51 -0700

    what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - function that prints it name
+ * @argc: parameter 1
+ * @argv: parameter 2
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (arg > 0)
+	{
+		printf("%s\n", arg[0]);
+	}
+	return (0);
+}
