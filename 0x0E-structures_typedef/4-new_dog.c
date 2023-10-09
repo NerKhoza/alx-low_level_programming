b642939c836aadfb83dc2a6a8f7ff1f52c765762
NerKhoza <Khoza.ner.142@gmail.com>
2023-10-09 15:59:43 -0700

    perpetually

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,39 @@
+#include "dog.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * *new_dog - function that creates a new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of the dog
+ * Return: NULL and newDog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *newDog = (dog_t *)malloc(sizeof(dog_t));
+
+	if (newDog == NULL)
+	{
+		return (NULL);
+	}
+
+	newDog->name = strdup(name);
+	newDog->owner = strdup(owner);
+
+	if (newDog->name == NULL || newDog->owner == NULL)
+	{
+		free(newDog->name);
+		free(newDog->owner);
+		free(newDog);
+		return (NULL);
+	}
+
+	newDog->age = age;
+
+	return (newDog);
+}
+
+
