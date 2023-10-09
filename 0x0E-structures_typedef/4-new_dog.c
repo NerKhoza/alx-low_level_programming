400529324e3c6827e04f7cfad86001c32a732fa6
NerKhoza <Khoza.ner.142@gmail.com>
2023-10-09 16:10:59 -0700

    perpetually

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -23,15 +23,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 	newDog->name = strdup(name);
 	newDog->owner = strdup(owner);
 
-	if (newDog->name == NULL || newDog->owner == NULL)
-	{
-		free(newDog->name);
-		free(newDog->owner);
-		free(newDog);
-		return (NULL);
-	}
-
 	newDog->age = age;
-
 	return (newDog);
 }
