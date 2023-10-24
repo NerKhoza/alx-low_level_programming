0ddd28669f2c05499935253514543d559f9396c4
NerKhoza <khoza.ner.142@gmail.com>
2023-10-24 10:56:19 -0700

    update

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint - function that frees a listint_t list
+ * @head: function parameter
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *current = head;
+	listint_t *temp;
+
+	while (current == NULL)
+	{
+		temp = current;
+		current = current->next;
+		free(temp);
+	}
+}
