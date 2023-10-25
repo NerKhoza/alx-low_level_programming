06e8bd4155dde36b342379687ea8f5175732bcd8
NerKhoza <khoza.ner.142@gmail.com>
2023-10-25 04:27:48 -0700

    update

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * printMessage - function that prints
+ */
+
+void printMessage(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
