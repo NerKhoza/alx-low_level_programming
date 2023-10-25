57807555c607fb806af31bd15de8295cc3b6d335
NerKhoza <khoza.ner.142@gmail.com>
2023-10-25 04:12:45 -0700

    update

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,8 +8,9 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+	size_t count;
 
+	count = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
@@ -17,9 +18,11 @@ size_t print_list(const list_t *h)
 			printf("[%d]%s\n", 0, "(nil)");
 		}
 		else
+		{
 			printf("[%d]%s\n", h->len, h->str);
-		h = h->next;
-		count++;
+		}
+			h = h->next;
+			count++;
 	}
 	return (count);
 }
